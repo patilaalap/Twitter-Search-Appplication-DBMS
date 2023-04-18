ba4732e32190774d6e212cbda3fbdd22d6b90d4a
Rushabh Karnavat <rpkarnavat@nbp-77-194.nbp.ruw.rutgers.edu>
2023-04-18 18:06:53 -0400

    Updated

--- Search.py
+++ Search.py
@@ -1,4 +1,6 @@
 from SQL_Read import UserDatabase
+from Mongo_Read import TweetQuery
+from Mongo_Read import TweetQuery_Hashtag
 #import ipywidgets as widgets
 from Cache import Cache
 
@@ -54,8 +56,40 @@ def retrieve_input():
         tk.mainloop()
     elif re.match(pattern_hashtag,x):
         print("# pattern")
+        hashtag_obj = TweetQuery_Hashtag()
+        tweets = hashtag_obj.search_tweets_by_hashtags(x[1:])
+        i = 0
+        for rows in tweets:
+            tweet = rows['text']
+            link = tk.Label(frame, text=tweet + "\n * * * * *", font=('Helveticabold', 15), fg="blue", cursor="hand2")
+            link.grid(row=i + 3, column=0)
+            # link.bind("<Button-1>", lambda event: user_details(event, uname))
+            # link = tk.Label(frame, text=rows[0], font=('Helveticabold', 15))
+            # link.grid(row=i + 3, column=1)
+            i = i + 1
+        # cache_obj.add_in_cache(x[1:], y)
+        # buttonCommit = tk.Button(frame, height=1, width=10, text="Show more", command=lambda: show_more(x, offset + 10))
+        # buttonCommit.grid()
+        tk.mainloop()
     else:
         print("Text")
+        tweet_obj = TweetQuery()
+        tweets = tweet_obj.query_tweets_by_regex(x)
+        i = 0
+        for rows in tweets:
+            t_id = rows['id_str']
+            link1 = tk.Label(frame, text=t_id, font=('Helveticabold', 15), fg="blue", cursor="hand2")
+            link1.grid(row=i + 3, column=0)
+            tweet = rows['text']
+            link2 = tk.Label(frame, text=tweet, font=('Helveticabold', 15))
+            link2.grid(row=i + 3, column=1)
+            link1.bind("<Button-1>", lambda event: tweet_details(event, t_id))
+            i = i + 1
+        #cache_obj.add_in_cache(x[1:], y)
+        #buttonCommit = tk.Button(frame, height=1, width=10, text="Show more", command=lambda: show_more(x, offset + 10))
+        #buttonCommit.grid()
+        tk.mainloop()
+
 
 def user_details(event, uname):
     clicked_label = event.widget
@@ -68,6 +102,18 @@ def user_details(event, uname):
                "   Following : "+str(rows[5])+"\nCreated at : "+str(rows[6])+"\n\n\nDescription : "+rows[3]
         tk.Label(frame, text=disp).grid(row=2)
     tk.mainloop()
+
+def tweet_details(event, t_id):
+    clicked_label = event.widget
+    label_text = clicked_label.cget("text")
+    clear_frame()
+    tweet_obj = TweetQuery()
+    tweet_det = tweet_obj.get_tweetdetails(label_text)
+    """for rows in user_det:
+        disp = "Name : "+rows[0]+"\nUsername : "+rows[1]+"\n Followers : "+str(rows[4])+\
+               "   Following : "+str(rows[5])+"\nCreated at : "+str(rows[6])+"\n\n\nDescription : "+rows[3]
+        tk.Label(frame, text=disp).grid(row=2)
+    tk.mainloop()"""
 
 def show_more(x,offset):
     clear_frame()
@@ -90,7 +136,7 @@ def show_more(x,offset):
 
 master = tk.Tk()
 master.title('Twitter search application')
-master.geometry("600x450")
+master.geometry("1920x1080")
 tk.Label(master, text='Search').grid(row=0)
 e1 = tk.Entry(master)
 e1.grid(row=1)
