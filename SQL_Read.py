210aa9397b9e0c94b1886363d82f036cb4315eda
Rushabh Karnavat <rpkarnavat@nbp-221-171.nbp.ruw.rutgers.edu>
2023-04-20 17:22:16 -0400

    Syntax

--- SQL_Read.py
+++ SQL_Read.py
@@ -1,13 +1,26 @@
-import sqlite3
+#import sqlite3
+import pymysql
 
 class UserDatabase:
     def __init__(self):
-        self.conn = sqlite3.connect('user_db.db')
+        self.conn = pymysql.connect(user='root',
+                              password='RushabhK',
+                              host='localhost',
+                              database = 'user_db',
+                              )
         self.cursor = self.conn.cursor()
+
 
     def get_user_by_name(self, username,offset):
         #sql = f"SELECT * FROM user WHERE name='{username}'"
-        sql = f"SELECT name,screen_name FROM user WHERE screen_name LIKE '{username}%' ORDER BY followers_count DESC LIMIT 10 OFFSET '{offset}'"
+        sql = f"SELECT name,screen_name FROM user WHERE screen_name LIKE '{username}%' ORDER BY followers_count DESC LIMIT 10 OFFSET {offset}"
+        self.cursor.execute(sql)
+        #self.cursor.execute("SELECT * FROM user WHERE id_str = '804046791348015107'")
+        result = self.cursor.fetchall()
+        return result
+
+    def get_top_ten(self):
+        sql = f"SELECT name, screen_name from user ORDER BY followers_count DESC LIMIT 10"
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return result
