75692b08ddcaa7e5ea92acae518212a826249176
patilaalap <patilaalap@gmail.com>
2023-04-25 20:38:44 -0400

    Added projection

--- Mongo_Read.py
+++ Mongo_Read.py
@@ -4,31 +4,31 @@ import re
 class TweetQuery:
     def __init__(self):
         self.client = pymongo.MongoClient("localhost", 27017)
-        self.db = self.client["twitter-covid2_updated"]
+        self.db = self.client["twitter-covid3"]
         self.coll_tweets = self.db["tweets"]
         self.coll_comments = self.db["comments"]
 
     def query_tweets_by_regex(self, regex_pattern):
         # Query tweets that match the regex pattern in the text field, ordered by popularity, and limit the results by 10
-        tweets = self.coll_tweets.find({"text": {"$regex": regex_pattern, "$options": "i"}}).sort("popularity", pymongo.DESCENDING).limit(10)
+        tweets = self.coll_tweets.find({"text": {"$regex": regex_pattern, "$options": "i"}}, {'_id':0}).sort("popularity", pymongo.DESCENDING).limit(10)
         return self.get_details_list(tweets)
 
     def search_tweets_by_hashtags(self, hashtags):
         query = {"hashtags": {"$in": [hashtags]}}
         limit = 10
-        tweets = self.coll_tweets.find(query).sort("popularity", pymongo.DESCENDING).limit(limit)
+        tweets = self.coll_tweets.find(query, {'_id':0}).sort("popularity", pymongo.DESCENDING).limit(limit)
         return self.get_details_list(tweets)
 
     def get_tweet_details(self, tweet_id):
-        tweet = self.coll_tweets.find({"id_str": tweet_id})
+        tweet = self.coll_tweets.find({"id_str": tweet_id}, {'_id':0})
         return self.get_details_list(tweet)
 
     def get_comments_for_tweet(self, tweet_id):
-        comments = self.coll_comments.find({"in_reply_to_status_id_str": tweet_id}).sort("created_at", pymongo.ASCENDING)
+        comments = self.coll_comments.find({"in_reply_to_status_id_str": tweet_id},{'id':0}).sort("created_at", pymongo.ASCENDING)
         return self.get_details_list(comments)
 
     def get_tweets_by_username(self, username):
-        tweets = self.coll_tweets.find({"user_name": username})
+        tweets = self.coll_tweets.find({"user_name": username},{'_id':0})
         return self.get_details_list(tweets)
 
     def get_top_10_popular_tweets(self):
