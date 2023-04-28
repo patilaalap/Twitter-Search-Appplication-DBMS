c034390b838254f209cff741d6aaa35ef6ce46ce
patilaalap <patilaalap@gmail.com>
2023-04-27 21:04:37 -0400

    Extracted hidden users

--- SQL_Insert.py
+++ SQL_Insert.py
@@ -1,111 +1,170 @@
+import json
+import sys
 import pymysql
 
 # establish connection to database
 conn = pymysql.connect(user='root',
-                              password='RushabhK',
+                              password='#Aalap21',
                               host='localhost',
                               database = 'user_db',
                               )
 cursor = conn.cursor()
-import pandas as pd
 
-covid2 = pd.read_json('corona-out-2',lines = True)
-
-
-df = pd.json_normalize(covid2['user'])
-# adding timestamp column
-df = df.assign(timestamp= covid2['timestamp_ms'])
-df['timestamp'] = df['timestamp'].astype(str)
-
-df = df.drop(['translator_type', 'protected', 'utc_offset', 'time_zone', 'geo_enabled', 'lang',
-                'contributors_enabled', 'is_translator', 'profile_background_color', 'profile_background_image_url',
-                'profile_background_image_url_https', 'profile_background_tile', 'profile_link_color',
-                'profile_sidebar_border_color', 'profile_sidebar_fill_color', 'profile_text_color',
-                'profile_use_background_image', 'profile_image_url', 'profile_image_url_https',
-                'profile_banner_url', 'default_profile', 'default_profile_image', 'following',
-                'follow_request_sent', 'notifications'], axis=1)
-
-#conn = sqlite3.connect('user_db.db')
-for i, row in df.iterrows():
-    # Define the SQL query to check if id already exists in the table
-    check_query = f"""
-    SELECT id_str, timestamp FROM user WHERE id_str={row['id_str']}
-    """
-    cursor.execute(check_query)
-    result = cursor.fetchone()
-
-    # If id doesn't exist, insert the row into the table
-    if result is None:
-        insert_query = """
-        INSERT INTO user (id_str, name, screen_name, location, url, description, verified, followers_count,
-                         friends_count, listed_count, favourites_count, statuses_count, created_at, timestamp)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-        """
-        cursor.execute(insert_query,
-                     (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
-                      row['description'], row['verified'], row['followers_count'], row['friends_count'],
-                      row['listed_count'], row['favourites_count'], row['statuses_count'], row['created_at'],
-                      row['timestamp']))
-        #cursor.commit()
-
-    # If id already exists and the new timestamp is greater, update the row
-    elif row['timestamp'] > result[1]:
-        update_query = """
-            UPDATE user SET id_str=%s, name=%s, screen_name=%s, location=%s, url=%s, description=%s, 
-            verified=%s, followers_count=%s, friends_count=%s, listed_count=%s, favourites_count=%s, 
-            statuses_count=%s, created_at=%s, timestamp=%s
-            WHERE id_str=%s
+line_ctr = 0
+with open("corona-out-3", "r") as f1:
+    for line in f1:
+        line_ctr += 1
+        try:
+            data = json.loads(line)
+            row = data['user']
+            # Define the SQL query to check if id already exists in the table
+            check_query = f"""
+            SELECT id_str, timestamp FROM user WHERE id_str={row['id_str']}
             """
-        cursor.execute(update_query, (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+            cursor.execute(check_query)
+            result = cursor.fetchone()
+
+            # If id doesn't exist, insert the row into the table
+            if result is None:
+                insert_query = """
+                INSERT INTO user (id_str, name, screen_name, location, url, description, verified, followers_count,
+                                 friends_count, listed_count, favourites_count, statuses_count, created_at, timestamp)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """
+                cursor.execute(insert_query,
+                             (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+                              row['description'], row['verified'], row['followers_count'], row['friends_count'],
+                              row['listed_count'], row['favourites_count'], row['statuses_count'], row['created_at'],
+                              data['timestamp_ms']))
+                #cursor.commit()
+
+            # If id already exists and the new timestamp is greater, update the row
+            elif data['timestamp_ms'] > result[1]:
+                update_query = """
+                    UPDATE user SET id_str=%s, name=%s, screen_name=%s, location=%s, url=%s, description=%s, 
+                    verified=%s, followers_count=%s, friends_count=%s, listed_count=%s, favourites_count=%s, 
+                    statuses_count=%s, created_at=%s, timestamp=%s
+                    WHERE id_str=%s
+                    """
+                cursor.execute(update_query, (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+                                            row['description'], row['verified'], row['followers_count'], row['friends_count'],
+                                            row['listed_count'], row['favourites_count'], row['statuses_count'],
+                                            row['created_at'],
+                                            data['timestamp_ms'], row['id_str']))
+                #conn.commit()
+
+            # If id already exists and the new timestamp is less than or equal to the old timestamp, skip the row
+            if 'retweeted_status' in data.keys():
+                row = data['retweeted_status']['user']
+                # Define the SQL query to check if id already exists in the table
+                check_query = f"""
+                            SELECT id_str, timestamp FROM user WHERE id_str={row['id_str']}
+                            """
+                cursor.execute(check_query)
+                result = cursor.fetchone()
+
+                # If id doesn't exist, insert the row into the table
+                if result is None:
+                    insert_query = """
+                                INSERT INTO user (id_str, name, screen_name, location, url, description, verified, followers_count,
+                                                 friends_count, listed_count, favourites_count, statuses_count, created_at, timestamp)
+                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                                """
+                    cursor.execute(insert_query,
+                                   (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
                                     row['description'], row['verified'], row['followers_count'], row['friends_count'],
                                     row['listed_count'], row['favourites_count'], row['statuses_count'],
                                     row['created_at'],
-                                    row['timestamp'], row['id_str']))
-        #conn.commit()
+                                    data['timestamp_ms']))
+                    # cursor.commit()
 
-    # If id already exists and the new timestamp is less than or equal to the old timestamp, skip the row
-    else:
-        continue
+                # If id already exists and the new timestamp is greater, update the row
+                elif data['timestamp_ms'] > result[1]:
+                    update_query = """
+                                    UPDATE user SET id_str=%s, name=%s, screen_name=%s, location=%s, url=%s, description=%s, 
+                                    verified=%s, followers_count=%s, friends_count=%s, listed_count=%s, favourites_count=%s, 
+                                    statuses_count=%s, created_at=%s, timestamp=%s
+                                    WHERE id_str=%s
+                                    """
+                    cursor.execute(update_query,
+                                   (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+                                    row['description'], row['verified'], row['followers_count'], row['friends_count'],
+                                    row['listed_count'], row['favourites_count'], row['statuses_count'],
+                                    row['created_at'],
+                                    data['timestamp_ms'], row['id_str']))
+                    # conn.commit()
+            if 'quoted_status' in data.keys():
+                row = data['quoted_status']['user']
+                # Define the SQL query to check if id already exists in the table
+                check_query = f"""
+                                            SELECT id_str, timestamp FROM user WHERE id_str={row['id_str']}
+                                            """
+                cursor.execute(check_query)
+                result = cursor.fetchone()
+
+                # If id doesn't exist, insert the row into the table
+                if result is None:
+                    insert_query = """
+                                                INSERT INTO user (id_str, name, screen_name, location, url, description, verified, followers_count,
+                                                                 friends_count, listed_count, favourites_count, statuses_count, created_at, timestamp)
+                                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                                                """
+                    cursor.execute(insert_query,
+                                   (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+                                    row['description'], row['verified'], row['followers_count'], row['friends_count'],
+                                    row['listed_count'], row['favourites_count'], row['statuses_count'],
+                                    row['created_at'],
+                                    data['timestamp_ms']))
+                    # cursor.commit()
+
+                # If id already exists and the new timestamp is greater, update the row
+                elif data['timestamp_ms'] > result[1]:
+                    update_query = """
+                                                    UPDATE user SET id_str=%s, name=%s, screen_name=%s, location=%s, url=%s, description=%s, 
+                                                    verified=%s, followers_count=%s, friends_count=%s, listed_count=%s, favourites_count=%s, 
+                                                    statuses_count=%s, created_at=%s, timestamp=%s
+                                                    WHERE id_str=%s
+                                                    """
+                    cursor.execute(update_query,
+                                   (row['id_str'], row['name'], row['screen_name'], row['location'], row['url'],
+                                    row['description'], row['verified'], row['followers_count'], row['friends_count'],
+                                    row['listed_count'], row['favourites_count'], row['statuses_count'],
+                                    row['created_at'],
+                                    data['timestamp_ms'], row['id_str']))
+                    # conn.commit()
+        except Exception as e:
+            if line_ctr%2 != 0:
+                print(line)
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print(exc_type, exc_tb.tb_lineno)
+                break
 
 #conn.commit()
 
-
-
-df2 = pd.json_normalize(covid2['retweeted_status'])
-retweets_df = covid2[['id_str']]
-retweets_df = retweets_df.assign(retweeted_status_id_str=df2['id_str'])
-retweets_df = retweets_df.assign(user_id_str=df['id_str'])
-retweets_df = retweets_df.assign(screen_name=df['screen_name'])
-retweets_df = retweets_df.assign(text=covid2['text'])
-
-retweets_df = retweets_df.dropna()
-retweets_df = retweets_df.drop_duplicates()
-
-# Creating the retweets table
-# Commit the changes and close the connection
-
-
-for index, row in retweets_df.iterrows():
-    # check if the first two characters of the text column are "RT"
-    if row['text'][0:2] == "RT":
-        # check if the row already exists in the retweets table
-        exists_query = "SELECT id_str FROM retweets WHERE id_str = %s"
-        cursor.execute(exists_query, (row['id_str'],))
-        exists = cursor.fetchone() is not None
-        if not exists:
-            # insert the record into the retweets table
-            insert_query = "INSERT INTO retweets VALUES (%s, %s, %s, %s)"
-            values = (row['id_str'], row['retweeted_status_id_str'],
-                      row['user_id_str'], row['screen_name'])
-            cursor.execute(insert_query, values)
-
-# commit the changes and close the connection
-
-
-
-# commit the changes and close the connection
-conn.commit()
-# commit the changes and close the connection
+line_ctr = 0
+with open("corona-out-3", "r") as f1:
+    for line in f1:
+        line_ctr += 1
+        try:
+            row = json.loads(line)
+            # check if the first two characters of the text column are "RT"
+            if row['text'][0:2] == "RT" and 'retweeted_status' in row.keys():
+                # check if the row already exists in the retweets table
+                exists_query = "SELECT id_str FROM retweets WHERE id_str = %s"
+                cursor.execute(exists_query, (row['id_str'],))
+                exists = cursor.fetchone() is not None
+                if not exists:
+                    # insert the record into the retweets table
+                    insert_query = "INSERT INTO retweets VALUES (%s, %s, %s, %s)"
+                    values = (row['id_str'], row['retweeted_status']['id_str'],
+                              row['user']['id_str'], row['user']['screen_name'])
+                    cursor.execute(insert_query, values)
+        except Exception as e:
+            if line_ctr%2 != 0:
+                print(line)
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                print(exc_type, exc_tb.tb_lineno)
+                break
 
 # commit the changes and close the connection
 conn.commit()
